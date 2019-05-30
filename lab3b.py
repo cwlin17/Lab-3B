f57a478c36aed9f3c7bef6dd4af3c8de7e9e3656
donle27 <donle22599@ucla.edu>
2019-05-30 13:45:20 -0700

    set up fillobjects function

--- lab3b.py
+++ lab3b.py
@@ -6,6 +6,14 @@
 
 import sys, csv
 
+def fillObjects():
+    with open(sys.argv[1], 'r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            firstCol = row[0]
+            if (firstCol == "INODE"):
+                print("Got an inode")
+    
 def main():
     # Checking for correct number of arguments
     if len(sys.argv) != 2:
@@ -16,9 +24,9 @@ def main():
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             print(",".join(row))
+    fillObjects()
             
 
 ##### Need to read csv file contents into data structure(s) #####
-
 if __name__ == "__main__":
     main()
