64dca9e6ec126e9fe769da13f58a6b0a5e2a146a
Carol Wu Lin <caroll@lnxsrv09.seas.ucla.edu>
2019-05-30 20:53:42 -0700

    classes

--- lab3b.py
+++ lab3b.py
@@ -6,6 +6,44 @@
 
 import sys, csv
 
+class SuperBlock:
+    def __init__(self, param):
+        self.totalNumBlocks = param[1]
+        self.totalNumInodes = param[2]
+        self.blockSize = param[3]
+        self.inodeSize = param[4]
+        self.blocksPerGroup = param[5]
+        self.inodesPerGroup = param[6]
+        self.firstNonresInode = param[7]
+
+class Group:
+    def __init__(self, param):
+        self.groupNum = int(param[1])
+        self.totalBlocksInGroup = int(param[2])
+        self.totalInodesInGroup = int(param[3])
+        self.numFreeBlocks = int(param[4])
+        self.numFreeInodes = int(param[5])
+        self.freeBlockBitmap = param[6]
+        self.freeInodeBitmap = param[7]
+        self.firstBlockInodes = param[8]
+
+class FreeBlockEntries:
+    def __init__(self, param):
+        self.freeBlockNum = int(param[1])
+
+class FreeInodeEntries:
+    def __init__(self, param):
+        self.freeInodeNum = int(param[1])
+
+class Inode:
+    def __init__(self, param):
+        self.inodeNum = int(param[1])
+        self.fileType = param[2]
+        self.owner = param[4]
+        self.group = param[5]
+        self.linkCount = int(param[6])
+        self.fileSize = int(param[10])
+
 def main():
     # Checking for correct number of arguments
     if len(sys.argv) != 2:
