7d0fdad469d884da599c307f572403fa748d1a06
Carol Wu Lin <caroll@lnxsrv09.seas.ucla.edu>
2019-05-31 20:34:39 -0700

    Inode allocation audits

--- lab3b.py
+++ lab3b.py
@@ -120,12 +120,42 @@ def main():
     findInodeInconsistencies()
 
     # Inode Allocation Audits
+    accountedNodes = [1, 3, 4, 5, 6, 7, 8, 9, 10]
+    # Check every allocated inode and see if it's also on free list
     for inode in inodeList:
-        type = inode.fileType
-        allocated = false
-        unallocated = false
-        if type == 0:
-            unallocated = true
+        accountedNodes.append(inode.inodeNum)
+        if (inode.fileType != 0):
+            for freeNode in freeInodeList:
+                if (freeNode.freeInodeNum == inode.inodeNum):
+                    print("ALLOCATED INODE " + str(inode.inodeNum) + " ON FREELIST")
+
+    for freeNode in freeInodeList:
+        accountedNodes.append(freeNode.freeInodeNum)
+
+    for i in range(1, superBlock.totalNumInodes + 1):
+        if i not in accountedNodes:
+            print("UNALLOCATED INODE " + str(i) + " NOT ON FREELIST") 
+
+        #if i in (1, 3, 4, 5, 6, 7, 8, 9, 10):
+            #continue
+        # Check every allocated inode and see if it's also on free list
+       # for inode in inodeList:
+        #    if (inode.fileType != 0 and inode.inodeNum == i):
+         #      for freeNode in freeInodeList:
+          #         if (freeNode.freeInodeNum == i):
+           #            print("ALLOCATED INODE " + str(i) + " ON FREELIST")
+                       
+    # Directory Consistency Audits
+    for inode in inodeList:
+        reference = inode.linkCount
+        number = inode.inodeNum
+        count = 0
+        for directory in directoryList:
+            parent = directory.parentInode
+            if (number == parent):
+                count += 1
+        if (count != reference):
+            print("INODE " + str(number) + " HAS " + str(count) + " LINKS BUT LINKCOUNT IS " + str(reference))
 
 if __name__ == "__main__":
     main()
